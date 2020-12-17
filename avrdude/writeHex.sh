d442972c569a5dcdfc5d78d3928062c78210986e
david <jonie4@gmail.com>
2020-12-17 06:32:20 -0800

    Fixed directory for avrdude command

--- avrdude/writeHex.sh
+++ avrdude/writeHex.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/sh
 
-avrdude -v -p atmega2560 -C /home/pi/.platformio/packages/tool-avrdude/avrdude.conf -c wiring -b 115200 -D -P "/dev/ttyACM0" -U flash:w:firmware.hex:i
+avrdude -v -p atmega2560 -C /home/pi/.platformio/packages/tool-avrdude/avrdude.conf -c wiring -b 115200 -D -P "/dev/ttyACM0" -U flash:w:/home/pi/Documents/3dprinter/Marlin-Firmware/.pio/build/mega2560/firmware.hex:i
 
